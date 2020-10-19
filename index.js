e31fc79b3691cb0c125a27d226f0a557e1506897
JossinAntony <jossinde@gmail.com>
2020-10-19 09:05:12 +0200

    write to file implemented.

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const myfs = require('fs');
+
 for(var i = 1; i <= 500; i++){
     if((i % 2) == 0){
         console.log(i);
+
+        myfs.appendFile('EvenNos1-500.txt', (i+'\n'), (e)=>{
+            if(e){
+                throw e;
+            }
+        })
     }
+
 }
+
